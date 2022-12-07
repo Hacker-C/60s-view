2879b1063185c882218d6230ea52bb19211d9f6b
Hacker-C <1507559148@qq.com>
2022-12-07 19:42:56 +0800

    💄 ui: update repo link

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,8 +3,8 @@ function Footer() {
     <footer flex-center font-sans pt-2 pb-7 text-white>
       <a href="https://mphy.me" target="_blank" mr-2>©MurphyChen 2022</a>
       |
-      <a href="https://github.com/hacker-c" target="_blank" ml-2>
-        Github/Hacker-C
+      <a href="https://github.com/hacker-c/60s-view" target="_blank" ml-2>
+        Github/Hacker-C/60s-view
       </a>
     </footer>
   )
