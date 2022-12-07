d2bebacb2380bb7ef4582054e60e0a751c848846
Hacker-C <1507559148@qq.com>
2022-12-07 19:51:44 +0800

    💄 ui: adjust font size

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
-    <footer flex-center font-sans pt-2 pb-7 text-white>
-      <a href="https://mphy.me" target="_blank" mr-2>©MurphyChen 2022</a>
+    <footer flex-center font-sans pt-2 pb-7 text="white sm">
+      <a href="https://mphy.me" target="_blank" mr-2>©MurphyChen</a>
       |
       <a href="https://github.com/hacker-c/60s-view" target="_blank" ml-2>
         Github/Hacker-C/60s-view
