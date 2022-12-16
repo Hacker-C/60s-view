87f1c3d3259dfa5bf8f128967365f135bdf4ab76
Hacker-C <1507559148@qq.com>
2022-12-16 20:55:20 +0800

    💄 ui: adjust page style

--- src/components/today-in-history.tsx
+++ src/components/today-in-history.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import historyData from '@/mock/history.json'
 
 interface HistoryType {
   year: string
@@ -16,6 +17,8 @@ function TodayInHistory({ theme }: { theme: string }) {
         const { data } = res
         setHistoryList(data.reverse())
       }).catch((e) => {
+        // 请求失败，使用 mock 数据
+        setHistoryList(historyData.data.reverse())
         console.log(e)
       })
   }, [])
@@ -25,10 +28,12 @@ function TodayInHistory({ theme }: { theme: string }) {
       <ul text-sm mt-2>
         { historyList.map((item) => {
           return (
-            <li key={item.title} className='leading-7'>
-              <span mr-2 font-sans><i>{item.year}</i></span>
-               ·
-              <span ml-2>{item.title}</span>
+            <li key={item.title} className='leading-6'>
+              <a href={item.link} target="_blank">
+                <span inline-block w-8 text-right font-sans><i>{item.year}</i></span>
+                <span mx-1>·</span>
+                <span>{item.title}</span>
+              </a>
             </li>
           )
         }) }
